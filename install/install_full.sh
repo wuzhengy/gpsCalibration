6d665ca4ee95990ca1a6c90876f10bea3159dd0d
taichenliu <taichen.liu@imorpheus.ai>
2017-09-11 19:27:42 +0800

    modify installation, using bash shell

--- install/install_full.sh
+++ install/install_full.sh
@@ -2,14 +2,11 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list';
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116;
 sudo apt-get update;
-sudo apt-get install -y ros-indigo-ros-base;
-sudo apt-get install -y ros-indigo-tf
-sudo apt-get install -y ros-indigo-opencv3;
-sudo apt-get install -y ros-indigo-pcl-conversions;
-sudo apt-get install -y ros-indigo-nav-msgs;
+sudo apt-get install -y ros-indigo-desktop-full;
 sudo rosdep init;
 sudo rosdep update;
-echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc; source ~/.bashrc;
+echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc;
+source ~/.bashrc;
 
 #PCL
 sudo add-apt-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl;
